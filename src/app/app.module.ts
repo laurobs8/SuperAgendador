991b8ea40d1a74dc03314baf1299f05d881bb826
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-11 22:21:44 -0200

    Arquivo de servico para conectar a api do github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GithubService } from './github.service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GithubService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/github.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class GithubService {
+
+    private username = 'laurobs8';
+
+    constructor(private http: HttpClient) { }
+
+    getUser() {
+        return this.http.get('https://api.github.com/users/' + this.username);
+        // .map(res => res.json());
+    }
+
+}
