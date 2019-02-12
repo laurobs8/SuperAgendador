5656dfd663b11a4176943865231597705524a2d8
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-11 22:20:32 -0200

    Mostrar componente criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'superagendador';
+
+  constructor(private gitHubService: GithubService) { }
+
 }
