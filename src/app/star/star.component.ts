8356b601f13e932b0916b89f160531e8ec23a6e7
Lauro-Soares <lauro_bs8@hotmail.com>
2019-02-13 11:55:56 -0200

    Rota para navegacao

---
+++ src/app/star/star.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.sass']
+})
+export class StarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
